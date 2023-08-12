c382b9dd6eaa5df686de16a49de383135463c6f4
Yousef Ahmed <yousef.ahmed@whitespacers.com>
2023-08-12 14:29:13 +0100

    Strip out unused containers and reduce Launches template

--- src/components/Launches/index.js
+++ src/components/Launches/index.js
@@ -1,16 +1,12 @@
-import {Grid, Paper} from "@mui/material";
+import {Grid} from "@mui/material";
 import Launch from "../Launch";
 import styles from './launches.module.css';
 
 const Launches = ({launches}) =>  {
     return (
-        <Grid container spacing={2}>
+        <Grid className={styles.launches} container spacing={2}>
             {launches.map((launch) => (
-                <Grid key={launch.id} item xs={4}>
-                    <Paper className={styles.page}>
-                        <Launch props={launch}/>
-                    </Paper>
-                </Grid>
+                <Launch key={launch.id} props={launch}/>
             ))}
         </Grid>
     );
